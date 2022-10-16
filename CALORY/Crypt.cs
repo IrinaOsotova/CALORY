1bc1117e6ed851a542217e8a7bbe8b502dc03bb1
Anro <anelnichenko@bk.ru>
2022-10-16 21:56:56 +0300

    Создал класс для хэширования паролей в БД

---
+++ CALORY/Crypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALORY
+{
+    public static class Crypt
+    {
+        public static string GetHashPassword(string password)
+        {
+            using var crypt = SHA256.Create();
+            var hash = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var hashString = new StringBuilder();
+            foreach (var x in hash)
+            {
+                hashString.Append(x.ToString("x2"));
+
+            }
+
+            return hashString.ToString();
+        }
+    }
+}
