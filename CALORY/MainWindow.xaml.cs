32d4ada96e4d2ba6aa0c2ca2c92d9959a70d7600
Anro <anelnichenko@bk.ru>
2022-10-16 22:20:40 +0300

    Создал окно авторизации

--- CALORY/MainWindow.xaml.cs
+++ CALORY/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 
 namespace CALORY
 {
@@ -10,6 +11,34 @@ namespace CALORY
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonComeInAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxLoginAuthorization.Text == "")
+            {
+                MessageBox.Show("Введите логин");
+                return;
+            }
+            if (PasswordBoxAuthorization.Password == "")
+            {
+                MessageBox.Show("Введите пароль");
+                return;
+            }
+            using (var db = new ApplicationContext())
+            {
+                var user = db.Users.FirstOrDefault(item => item.login == TextBoxLoginAuthorization.Text && item.password == Crypt.GetHashPassword(PasswordBoxAuthorization.Password));
+                if (user == null)
+                {
+                    MessageBox.Show("Не правильно введен логин или пароль");
+                }
+                else
+                {
+                    Diary window = new Diary();
+                    window.Show();
+                    Close();
+                }
+            }
         }
     }
 }
